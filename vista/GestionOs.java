d9fedfb5bd8c9d97445efd2cc53d54957eafaf96
workstation <mr_atomo@pm.me>
2023-10-24 19:14:32 +0200

    //Version inicial de estructura//

---
+++ vista/GestionOs.java
@@ -0,0 +1,54 @@
+package NextGen.vista;
+
+import NextGen.controlador.Controlador;
+import java.util.Scanner;
+
+public class GestionOS {
+    private Controlador controlador;
+    Scanner teclado = new Scanner(System.in);
+
+    public GestionOS() {
+        controlador = new Controlador();
+    }
+
+    public void inicio() {
+        boolean salir = false;
+        char opcio;
+        do {
+            System.out.println("1. Gestión Articulos");
+            System.out.println("2. Gestión Clientes");
+            System.out.println("3. Gestión Pedidos");
+            System.out.println("0. Salir");
+            opcio = pedirOpcion();
+            switch (opcio) {
+                case '1':
+                    controlador.listarArticulos();
+                    // Otras operaciones relacionadas con artículos
+                    break;
+                case '2':
+                    controlador.listarClientes();
+                    // Otras operaciones relacionadas con clientes
+                    break;
+                case '3':
+                    controlador.listarPedidos();
+                    // Otras operaciones relacionadas con pedidos
+                    break;
+                case '0':
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida. Por favor, elija una opción válida.");
+            }
+        } while (!salir);
+    }
+
+    private char pedirOpcion() {
+        String resp;
+        System.out.println("Elige una opción (1,2,3 o 0): ");
+        resp = teclado.nextLine();
+        if (resp.isEmpty()) {
+            resp = " ";
+        }
+        return resp.charAt(0);
+    }
+}
---
+++ vista/OnlineStore.java
@@ -0,0 +1,8 @@
+package NextGen.vista;
+
+public class OnlineStore {
+    public static void main(String[] args) {
+        GestionOS gestion = new GestionOS();
+        gestion.inicio();
+    }
+}
