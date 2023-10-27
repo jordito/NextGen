5e0d85591f8bf8d471a085c828316afae3d430e8
gegexina <115779572+gegexina@users.noreply.github.com>
2023-10-27 13:08:59 +0200

    He añadido documentación y algunos metodos, comprobar si está todo ok y si funciona.

--- NextGen/modelo/ClientePremium.java
+++ NextGen/modelo/ClientePremium.java
@@ -1,27 +1,64 @@
 package NextGen.modelo;
-
+/**
+ * Clase que representa un cliente premium. Hereda de la clase abstracta Cliente.
+ */
 public class ClientePremium extends Cliente {
-    //Atributos
     private double cuota;
     private double descuento;
+    /**
+     * Constructor para crear un cliente premium con los detalles especificados.
+     * @param nif El NIF del cliente.
+     * @param nombre El nombre del cliente.
+     * @param email El correo electrónico del cliente.
+     * @param direccion La dirección de envío del cliente.
+     */
     public ClientePremium(String nif, String nombre, String email, String direccion) {
         super(nif, nombre, email, direccion);
         this.cuota = calcAnual();
         this.descuento = descuentoEnv();
     }
-    //Getters and Setters
+    /**
+     * Obtiene la cuota anual del cliente premium.
+     * @return La cuota anual del cliente premium.
+     */
     public double getCuota() { return cuota;}
+    /**
+     * Establece la cuota anual del cliente premium.
+     * @param cuota La nueva cuota anual del cliente premium.
+     */
     public void setCuota(double cuota) { this.cuota = cuota;}
+    /**
+     * Obtiene el descuento en el envío aplicado al cliente premium.
+     * @return El descuento en el envío como un valor entre 0 y 1.
+     */
     public double getDescuento() {return descuento; }
+    /**
+     * Establece el descuento en el envío del cliente premium.
+     * @param descuento El nuevo descuento en el envío del cliente premium.
+     */
     public void setDescuento(double descuento) {this.descuento = descuento;}
 
-    //Metodos abstractos
+    /**
+     * Obtiene el tipo de cliente.
+     * @return El tipo de cliente, que es "Premium" para esta clase.
+     */
     public String tipoCliente() {
         return "Premium";
     }
+    /**
+     * Calcula la cuota anual del cliente premium. En este caso, siempre devuelve 30.
+     * @return La cuota anual del cliente premium, que es 30€.
+     */
     public float calcAnual() { return 30; }
+    /**
+     * Calcula el descuento en el envío para el cliente premium. En este caso, siempre devuelve 0.2.
+     * @return El descuento en el envío como un valor entre 0 y 1, que es 0.2.
+     */
     public float descuentoEnv() { return 0.2; }
-
+    /**
+     * Representación en forma de tabla del cliente premium con los detalles y cuota anual.
+     * @return Una cadena que muestra los detalles del cliente premium en forma de tabla, junto con su cuota anual y descuento en envío.
+     */
     @Override
     public String toString() {
         return super.toString() +
--- NextGen/modelo/ListaPedidos.java
+++ NextGen/modelo/ListaPedidos.java
@@ -1,5 +1,33 @@
 package NextGen.modelo;
-
+/**
+ * Clase que representa una lista de pedidos. Esta clase hereda de la clase genérica Lista<Pedido>.
+ */
 public class ListaPedidos extends Lista<Pedido> {
-    // Funcionalidades específicas para ListaPedidos, si las hay
+    /**
+     * Constructor para la lista de pedidos. Inicializa una lista de pedidos vacía.
+     */
+    public ListaPedidos() {
+        super();
+    }
+    /**
+     * Busca un pedido en la lista por su número de pedido.
+     * @param numeroPedido El número del pedido que se desea buscar.
+     * @return El pedido con el número especificado, o null si no se encuentra.
+     */
+    public Pedido buscarPorNumeropedido(int numeroPedido) {
+        for (Pedido pedido : lista) {
+            if (cliente.getNumeroPedido() == numeroPedido) {
+                return numeroPedido;
+            }
+        }
+        return null;
+    }
+    /**
+     * Método para agregar un pedido a la lista
+     * @param numeroPedido El pedido que se desea agregar a la lista
+     */
+    public void agregarPedido(Pedido pedido) {
+        lista.add(pedido);
+    }
+
 }
