1a8391729cf008d78bdd2719f17873ee42cd01d8
Jordi <72454521+jordito@users.noreply.github.com>
2023-12-17 11:58:10 +0100

    Update ArticuloDAO

--- NextGen/src/main/java/local/NextGen/modelo/DAO/ArticuloDAO.java
+++ NextGen/src/main/java/local/NextGen/modelo/DAO/ArticuloDAO.java
@@ -15,66 +15,68 @@ public class ArticuloDAO {
 
         List<Articulo> articulos = new ArrayList<>();
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<Articulo> query = session.createQuery("FROM Articulo", Articulo.class);
-            articulos = query.list();
-
+            try {
+                Query<Articulo> query = session.createQuery("FROM Articulo", Articulo.class);
+                articulos = query.list();
+            } catch (Exception e) {
+                // Mostramos el error
+                throw new DAOException("Error al buscar los usuarios", e);
+            } finally {
+                // Cerrar la sesión
+                session.close();
+            }
         } catch (Exception e) {
-            throw new DAOException("Error al buscar los usuarios ", e);
+            // Mostramos el error
+            throw new DAOException("Error al conectar con la base de datos", e);
         }
         return articulos;
     }
 
     public Articulo obtenerPorCodigo(String codigo) {
-        // Obtenemos la sesión de hibernate a partir de la configuración
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
-        // Abrimos la sesión
-        Session session = sessionFactory.openSession();
-
         Articulo articulo = null;
+        // Obtenemos la sesión de hibernate a partir de la configuración y abrimos la sesión
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            System.out.println("Código ingresado: " + codigo);
 
-        System.out.println("Código ingresado: " + codigo);
-
-        try {
-            articulo = session.get(Articulo.class, codigo);
+            try {
+                articulo = session.get(Articulo.class, codigo);
+            } catch (Exception e) {
+                // Mostramos el error
+                throw new DAOException("Error al buscar el usuario con ID " + codigo, e);
+            } finally {
+                // Cerrar la sesión
+                session.close();
+            }
         } catch (Exception e) {
-            // Handle exceptions or log the error
+            // Mostramos el error
             throw new DAOException("Error al buscar el usuario con ID " + codigo, e);
-        } finally {
-            // Cerrar la sesión
-            session.close();
         }
-
-        // Cerramos el session factory
-        HibernateUtil.shutdown();
 
         return articulo;
     }
 
 
     public boolean insertar(Articulo articulo) {
-        // Obtenemos la sesión de hibernate a partir de la configuración
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
-        // Abrimos la sesión
-        Session session = sessionFactory.openSession();
+        // Inicializamos la transacción
         Transaction tx = null;
+        // Obtenemos la sesión de hibernate a partir de la configuración y abrimos la sesión
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            try {
+                tx = session.beginTransaction();
+                session.persist(articulo);
+                tx.commit();
+            } catch (Exception e) {
+                // Mostramos el error
+                throw new DAOException("Error al insertar el artículo", e);
 
-        try {
-            tx = session.beginTransaction();
-            session.persist(articulo);
-            tx.commit();
+            } finally {
+                // Cerrar la sesión
+                session.close();
+            }
         } catch (Exception e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-            return false;
-        } finally {
-            // Cerrar la sesión
-            session.close();
+            // Mostramos el error
+            throw new DAOException("Error al conectar con la base de datos", e);
         }
-
-        // Cerramos el session factory
-        HibernateUtil.shutdown();
         return true;
     }
 
